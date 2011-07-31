c586e4015c259768fa1cf3c50b37fa644e92bb37
Alan Harper <alan@aussiegeek.net>
2011-07-31 20:24:36 +1000

    Initial Commit

---
+++ MagicalRecordTest/MagicalRecordTestAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MagicalRecordTestAppDelegate.h
+//  MagicalRecordTest
+//
+//  Created by Alan Harper on 31/07/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MagicalRecordTestAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MagicalRecordTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MagicalRecordTest
+//
+//  Created by Alan Harper on 31/07/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MagicalRecordTestAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MagicalRecordTestAppDelegate class]));
+    }
+}
